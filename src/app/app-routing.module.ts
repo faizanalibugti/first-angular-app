238c7329d57d5169625fcb0876764b86c5514eda
Faizan Ali Bugti <57310345+faizanalibugti@users.noreply.github.com>
2021-11-04 19:46:44 +0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/customers'}
+  { path: '', pathMatch: 'full'}
+//   { path: '', pathMatch: 'full', redirectTo: '/customers'}
   // { path: '**', pathMatch: 'full', redirectTo: '/customers'}
 ];
 
